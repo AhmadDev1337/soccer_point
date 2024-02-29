39fec088cb33707605b6577239fba9bee002b03e
Ahmat Ramadana Rayyan <ahmadxploit@gmail.com>
2024-03-01 02:43:23 +0700

    second version

--- lib/home/bar_controller.dart
+++ lib/home/bar_controller.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import 'account.dart';
 import 'home_page.dart';
 import 'statistics.dart';
 
@@ -40,6 +41,7 @@ class _BarControllerState extends State<BarController> {
             children: [
               HomePage(),
               StatisticsPage(),
+              AccountPage(),
             ],
           ),
           Positioned(
